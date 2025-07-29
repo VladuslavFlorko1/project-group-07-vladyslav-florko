a4a4b38c4b7142be086884f5622f148e37fd4376
Vladyslav Florko <florkovlad@email.com>
2025-07-29 14:09:27 +0300

    добаваив зображення

---
+++ src/modal-80.js
@@ -0,0 +1,15 @@
+(() => {
+    const refs = {
+      openModalBtn: document.querySelector("[data-modal-open-80]"),
+      closeModalBtn: document.querySelector("[data-modal-close-80]"),
+      modal: document.querySelector("[data-modal-80]"),
+    };
+  
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle("is-hidden");
+      document.body.classList.toggle("no-scroll");
+    }
+  })();
